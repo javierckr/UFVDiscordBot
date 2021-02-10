98cf6851af5289c1b8d8893c946764b665ef781e
javierckr <holajao23@gmail.com>
2021-02-10 10:39:24 +0100

    Cambiadas las variables de entorno a un .env

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 import discord
 import os
 import dia_semana
-# from _token import DISCORD_TOKEN
 
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('Iniciado con cuenta: {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -18,7 +19,7 @@ async def on_message(message):
     if message.content.startswith(',buses'):
         await message.channel.send('--- Los horarios de los buses son: --- \n1234')
     elif message.content.startswith(',dia'):
-        await message.channel.send(dia_de_la_semana())
+        await message.channel.send(dia_semana())
 
 
-client.run(DISCORD_TOKEN)
+client.run(os.getenv('DISCORD_TOKEN'))
