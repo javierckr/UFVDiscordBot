81a2aa534549e5fa7f662883b5c9fd1c4a1d0e8a
javierckr <holajao23@gmail.com>
2021-02-13 19:09:51 +0100

    Añadida la ayuda de los comandos

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 
 # Comando para llamar al bot
-bot = commands.Bot(command_prefix=',')
+bot = commands.Bot(command_prefix=',', description='Bot de discord de la UFV')
 
 
 @bot.event
@@ -18,21 +18,25 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
+    """ Comando para hacer ping al bot """
     await ctx.send('pong')
 
 
 @bot.command()
 async def buses(ctx):
+    """ Comando que dice el horario de los buses """
     await ctx.send('-- Los horarios del bus 659 son: a--\n 8:44 am ')
 
 
 @bot.command()
 async def dia(ctx):
+    """ Comando que dice las asignaturas del dia """
     await ctx.send(funciones.dia_semana.dia_de_la_semana())
 
 
 @bot.command()
 async def tareas(ctx):
+    """ Comando que te muestra las 10 siguientes tareas del calendario de las universidad """
     await ctx.send(funciones.googlecal.main())
 
 
